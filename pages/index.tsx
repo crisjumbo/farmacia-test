80ae61e4ff3109b69c657ff01f074168d6c1f4fe
aybike <tonn.raus09@gmail.com>
2021-10-29 15:51:50 +0200

    feat: nextjs and chakra setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import {Box, Text} from '@chakra-ui/layout'
+
+const Home: NextPage = () => {
+  return (
+  <Box>
+    <Text>Hola mundo</Text>
+  </Box>
+  )
+}
+
+export default Home

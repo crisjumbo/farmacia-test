a2c467078fd5731a31aa27c520819e3b86333db1
aybike <tonn.raus09@gmail.com>
2021-10-31 00:48:59 +0200

    feat: add product page details

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,16 +1,48 @@
 import React from 'react'
 import {useRouter} from 'next/router'
 import {Box, Text} from '@chakra-ui/layout'
+import { Image } from '@chakra-ui/image'
 
-const Product= () => {
+
+
+const Product= ({itemInfo}:any) => {
     const router = useRouter();
     const { id } = router.query;
+    console.log(itemInfo)
     return (
     <Box>
-        <Text>Hello Product {id}</Text>
+        <Text>{itemInfo.name}</Text>
+        <Text>{itemInfo.description}</Text>
+        <Text>{itemInfo.price}</Text>
+        {
+            itemInfo.product_colors.map((color:any, i:number) => (
+            <Box key={i}>
+                <Text>{color.hex_value}</Text>
+                <Text>{color.colour_name}</Text>
+            </Box>
+            ))
+        }
+        {
+            itemInfo.tag_list.map((tag:string, i:number) => (
+                <Text key={i}>{tag}</Text>
+            ))
+        }
+        <Image src={itemInfo.image_link} alt={itemInfo.name}/>
+        <Image src={itemInfo.api_feature_image} alt={itemInfo.name}/>
     </Box>
     )
 }
 
 export default Product;
 
+export const getServerSideProps = async ({params}:{params:{id?:string | number}}) => {
+    const endpoint:string = process.env.NEXT_PUBLIC_ITEM || '';
+    const res = await fetch(`${endpoint}${params.id}.json`)
+    const itemInfo= await res.json()
+    
+    return {
+        props: {
+            itemInfo
+        }
+    }
+}
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -3,21 +3,24 @@ import {Box, Heading, Text} from '@chakra-ui/layout';
 import {Image} from '@chakra-ui/image'
 import { Product } from '../interfaces/products';
 import {BsStar} from 'react-icons/bs'
+import Link from 'next/link'
 
-const  Item = ({ image_link, name, price, rating}:Product) => {
+const  Item = ({ image_link, name, price, rating, id}:Product) => {
     return (
-    <Box h="20rem" display="grid" gridTemplateRows="70% 30%" cursor="pointer">
-      <Box>
-        <Image src={'hola'} alt={name} h="100%" w="100%" objectFit="cover"/>
+    <Link href={`products/${id}`} passHref={true}>
+      <Box h="20rem" display="grid" gridTemplateRows="70% 30%" cursor="pointer">
+        <Box>
+          <Image src={'hola'} alt={name} h="100%" w="100%" objectFit="cover"/>
+        </Box>
+        <Box px="10px" pos="relative">
+          <Heading pos="relative" h="3.2rem" top="5px" as="h3" fontSize="20px" mb="5px">{name}</Heading>
+          <Text pos="relative" bottom ="0px" display="flex" justifyContent="space-between">
+            <Text as="span" >{price}&nbsp;$</Text>
+            <Text alignItems="center" display="flex" as="span">{rating ?rating :'No rate'}&nbsp;{rating && <BsStar/>}</Text>
+          </Text>
+        </Box>
       </Box>
-      <Box px="10px" pos="relative">
-        <Heading pos="relative" h="3.2rem" top="5px" as="h3" fontSize="20px" mb="5px">{name}</Heading>
-        <Text pos="relative" bottom ="0px" display="flex" justifyContent="space-between">
-          <Text as="span" >{price}&nbsp;$</Text>
-          <Text alignItems="center" display="flex" as="span">{rating ?rating :'No rate'}&nbsp;{rating && <BsStar/>}</Text>
-        </Text>
-      </Box>
-    </Box>
+    </Link>
     )
 }
 

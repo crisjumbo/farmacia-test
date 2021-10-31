8ff1a3b8dd915c53145dc16c224b03e68fb01046
aybike <tonn.raus09@gmail.com>
2021-10-31 10:12:05 +0100

    feat: craete simple pdp template

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,34 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useRouter} from 'next/router'
-import {Box, Text} from '@chakra-ui/layout'
+import {Box, Text, HStack, Heading} from '@chakra-ui/layout'
 import { Image } from '@chakra-ui/image'
+import { Tooltip } from '@chakra-ui/tooltip'
+import { Button } from '@chakra-ui/button'
 
 
 
 const Product= ({itemInfo}:any) => {
     const router = useRouter();
     const { id } = router.query;
-    console.log(itemInfo)
+
     return (
-    <Box>
-        <Text>{itemInfo.name}</Text>
-        <Text>{itemInfo.description}</Text>
-        <Text>{itemInfo.price}</Text>
-        {
-            itemInfo.product_colors.map((color:any, i:number) => (
-            <Box key={i}>
-                <Text>{color.hex_value}</Text>
-                <Text>{color.colour_name}</Text>
+    <Box display="flex" h="100vh" bg="blue" w="100vw" p="1rem" alignItems="center">
+        <Box>
+            <Box as="figcaption">
+                <Image boxSize="30rem" objectFit="fill" src={itemInfo.image_link} alt={itemInfo.name}/>
             </Box>
-            ))
-        }
-        {
-            itemInfo.tag_list.map((tag:string, i:number) => (
-                <Text key={i}>{tag}</Text>
-            ))
-        }
-        <Image src={itemInfo.image_link} alt={itemInfo.name}/>
-        <Image src={itemInfo.api_feature_image} alt={itemInfo.name}/>
+        </Box>
+        <Box px="1rem">
+            <Box>
+                <Heading as="h1">{itemInfo.name}</Heading>
+                <Text>{itemInfo.description}</Text>
+            </Box>
+            <Text my="1rem">
+                <Text as="span">{itemInfo.currency}</Text>
+                <Text as="span">&nbsp;{itemInfo.price}&nbsp;</Text>
+                <Text as="span">{itemInfo.price_sign}</Text>
+            </Text>
+            <HStack>
+            {
+                itemInfo.product_colors.map((color:any, i:number) => (
+                <Tooltip key={i} label={color.colour_name}>
+                    <Button bg={color.hex_value} borderRadius="50%"/>
+                </Tooltip>
+                ))
+            }
+            </HStack>
+        </Box>
     </Box>
     )
 }
